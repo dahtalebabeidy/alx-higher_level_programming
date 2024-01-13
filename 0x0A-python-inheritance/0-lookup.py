54a528d58a6ca73b30ede1cb113874fc8b67506f
dahtalebabeidy <dahtalebabeidy@gmail.com>
2024-01-13 15:20:41 +0000

    First

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+"""module for lookup method."""
+def lookup(obj):
+    """list object atributs and methods
+    Args:
+	obj: object
+    Return:
+	list of atributs and methods
+    """
+    return dir(obj)
