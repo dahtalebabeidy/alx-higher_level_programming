94eb01130a4b0f96cb07f400c03faff99e33a60a
dahtalebabeidy <dahtalebabeidy@gmail.com>
2024-01-20 14:55:55 +0000

    commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,6 +9,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-def to_json(self):
-    """return __dict__"""
-    return self.__dict__
+    def to_json(self):
+        """return __dict__"""
+        return self.__dict__
