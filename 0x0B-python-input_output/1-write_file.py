ace84e807f89be994bdbe28dee89cf9e55671310
dahtalebabeidy <dahtalebabeidy@gmail.com>
2024-01-20 00:40:58 +0000

    Dah

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """module for write_file function"""
+
+
 def write_file(filename="", text=""):
-	"""writes text in filename"""
-	with open(filename, 'w', encoding = 'utf-8') as file:
-		return file.write(text)
+    """writes text in filename"""
+    with open(filename, 'w', encoding = 'utf-8') as file:
+        return file.write(text)
