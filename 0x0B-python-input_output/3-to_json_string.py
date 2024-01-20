5ea7e8e013d5b66764f5768512607b9267fd9adf
dahtalebabeidy <dahtalebabeidy@gmail.com>
2024-01-20 13:35:31 +0000

    commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,5 @@ import json
 """import json module"""
 
 def to_json_string(my_obj):
-    """return my_obj json string"""
-    return json.dumps(my_obj)
+	"""return my_obj json string"""
+	return json.dumps(my_obj)
