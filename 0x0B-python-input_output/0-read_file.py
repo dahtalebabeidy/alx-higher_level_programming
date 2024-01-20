ce393e63738d3c9de6bce3333264391ef5ec2817
dahtalebabeidy <dahtalebabeidy@gmail.com>
2024-01-20 00:26:55 +0000

    Dah

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-	with open(filename, encoding='utf-8') as file:
-		f = file.read()
-		print(f, end="")
+	with open(filename, encoding='utf-8') as f:
+		print(f.read(), end="")
